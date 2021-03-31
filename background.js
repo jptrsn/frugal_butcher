155f3bce1284076fe27b92d190ddae970b4274e6
jptrsn <ptrsnja@gmail.com>
2021-03-31 00:38:20 -0400

    multi-click actions

--- background.js
+++ background.js
@@ -1,48 +1,119 @@
-
+let meetCommands;
+let pendingCommand;
 
 chrome.runtime.onInstalled.addListener(() => {
     console.log('onInstalled');
 });
 
-chrome.commands.onCommand.addListener((command) => {
+chrome.commands.onCommand.addListener(async (command) => {
     console.log('command', command);
+    if (meetCommands) {
+        const result = await meetCommands.executeCommand(command);
+        console.log('result', result);
+    } else if (command === 'newMeeting' || command === 'firstEvent') {
+        console.log('meet tab not initialized = searching...');
+        const tabs = await chrome.tabs.query({url: 'https://meet.google.com/*'})
+        if (tabs.length === 1) {
+            meetCommands = new MeetCommands(tabs[0]);
+            return meetCommands.executeCommand(command);
+        } else {
+            console.log('opening tab');
+            pendingCommand = command;
+            const tab = await chrome.tabs.create({url: 'https://meet.google.com'});
+            console.log('tab', tab);
+            
+        }
+    }
 });
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     console.log('message', message, sender);
     switch (message.action) {
         case 'addTab': {
-            meetCommands = new MeetCommands();
-            meetCommands.addPort(sender.tab);
+            if (!meetCommands) {
+                meetCommands = new MeetCommands(sender.tab);
+                if (pendingCommand) {
+                    console.log(`issuing pending command ${pendingCommand}`);
+                    const result = await meetCommands.executeCommand(pendingCommand);
+                    console.log('executeCommand result', result);
+                    pendingCommand = null;
+                }
+            }
             break;
         }
     }
+    sendResponse({success: true});
 })
 
 class MeetCommands {
     port;
-    constructor() {
+    clickSequences = {
+        newMeeting: ['newMeeting.firstEvent'],
+        firstEvent: ['firstEvent'],
+        toggleCaptions: ['toggleCaptions']
+    };
+    constructor(tab) {
         console.log('MeetCommands constructor');        
+        this.addPort(tab);
     }
 
-    async addPort(tab) {
+    addPort(tab) {
         this.port = chrome.tabs.connect(tab.id, {name: 'MeetCommands'});
-        
         this.port.onDisconnect.addListener(() => {
             console.log('disconnected');
             delete this.port;
         });
-        
-        const response = await this.sendMessage_({action: 'added'});
-        console.log('response', response);
+        this.sendMessage_({action: 'added'});
+    }
+
+    async backoff_(fn, retries = 5, delay = 500) {
+        let attempt = 0;
+        let e;
+        while (attempt < retries) {
+            try {
+                return await fn();
+            } catch(error) {
+                e = error;
+                console.warn(e);
+                attempt++;
+                const timeout = delay * 2 ** attempt;
+                console.log(`attempt ${attempt} in ${timeout} ms`);
+                await new Promise((resolve, reject) => setTimeout(resolve, timeout));
+            }
+        }
+        console.error('backoff exceeded', e);
+        throw new Error(e);
+    }
+
+    async executeCommand(command) {
+        const clickSequence = this.clickSequences[command];
+        for (let click of clickSequence) {
+            console.log('executing click', click);
+            const result = await this.sendMessage_({command: click});
+            console.log('result', result);
+        }
     }
 
     sendMessage_(message) {
-        return new Promise((resolve, reject) => {
-            this.port.onMessage.addListener((message, port) => {
-                resolve(message);
-            });
-            this.port.postMessage(message);
-        })
+        const send = () => {
+            return new Promise((resolve, reject) => {
+                if (!this.port) return {success: false};
+                const key = new Date().getTime();
+                const callback = (message, port) => {
+                    if (message.key === key) {
+                        delete message.key;
+                        port.onMessage.removeListener(callback);
+                        if (message.success) {
+                            resolve(message);
+                        } else {
+                            reject(message);
+                        }
+                    }
+                };
+                this.port.onMessage.addListener(callback);
+                this.port.postMessage(Object.assign({key}, message));
+            })
+        };
+        return this.backoff_(send, 5);
     }
 }
--- meet-actions.js
+++ meet-actions.js
@@ -1,29 +1,62 @@
 class MeetActions {
     port;
     currentState;
+    // State detectors are functions that return true if they have detected the state (as indicated by the key)
+    stateDetectors = {
+        hold: () => {
+            return !!document.querySelector('c-wiz > div > div > div:nth-child(4) > div.crqnQb > div > div.vgJExf > div > div > div.oORaUb.NONs6c');
+        },
+        meet: () => {
+            return !!document.querySelector('[data-allocation-index]');
+        },
+        home: () => {
+            return !!document.querySelector('#yDmH0d > c-wiz > div > div.S3RDod > div > div.Qcuypc > div.Ez8Iud');
+        }
+    };
+    // Each control key must correspond to the state, as determined by state detectors
+    // Each page of controls has a selector, when found it will have an element, and optionally has children that are injected into the DOM on click
     controls = {
         home: {
-            'newMeeting': {
+            newMeeting: {
                 selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div > button',
+                element: null,
                 children: {
                     'createForLater': {
-                        selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > ul > li[aria-label="Create a meeting for later"]',
+                        selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > ul > li:nth-child(1)',
                     },
                     'startInstant': {
-                        selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > ul > li[aria-label="Start an instant meeting"]',
+                        selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > ul > li:nth-child(2)',
                         element: null
                     },
                     'startInstant': {
-                        selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > ul > li[aria-label="Schedule in Google Calendar"]',
+                        selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > ul > li:nth-child(3)',
                         element: null
                     }
                 }
             },
-            'firstEvent': {
-                selector: 'document.querySelector("c-wiz > div > div:nth-child(2) > div > div:nth-child(2) > c-wiz > c-wiz > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(5) > c-wiz > div > c-wiz:nth-child(1) > div")'
+            firstEvent: {
+                selector: 'c-wiz > div > div:nth-child(2) > div > div:nth-child(2) > c-wiz > c-wiz > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(5) > c-wiz > div > c-wiz:nth-child(1) > div > div',
+                element: null
             }
         },
-        hold: {},
+        hold: {
+            joinNow: {
+                selector: '#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div.KieQAe > div.d7iDfe.NONs6c > div > div:nth-child(2) > div > div:nth-child(1) > div:nth-child(1)',
+                element: null
+            },
+            present: {
+                selector: '#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div.KieQAe > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div.uArJ5e.UQuaGc.kCyAyd.QU4Gid.xKiqt.cd29Sd.M9Bg4d',
+                element: null
+            },
+            cast: {
+                selector: '#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div.KieQAe > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.kgb0ld > div',
+                element: null
+            },
+            changeBackground: {
+                selector: '#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div.KieQAe > div.ZUpb4c > div.oORaUb.NONs6c > div > div.Rd6sU > div',
+                element: null
+            }
+        },
         meet: {
             'raiseHand': {
                 selector: '[aria-label="Raise hand"]',
@@ -39,24 +72,31 @@ class MeetActions {
             },
             'moreActions': {
                 selector: 'c-wiz > div > div > div:nth-child(9) > div:nth-child(3) > div:nth-child(9) > div:nth-child(3) > div:nth-child(4) > div',
+                element: null,
                 children: {
                     'cast': {
-                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(1)'
+                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(1)',
+                        element: null
                     },
                     'whiteboard': {
-                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(2)'
+                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(2)',
+                        element: null
                     },
                     'changeLayout': {
-                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(3)'
+                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(3)',
+                        element: null
                     },
                     'fullScreen': {
-                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(4)'
+                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(4)',
+                        element: null
                     },
                     'changeBackground': {
-                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(5)'
+                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(5)',
+                        element: null
                     },
                     'settings': {
-                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(11)'
+                        selector: 'body > div.JPdR6b.e5Emjc.CIYi0d.jvUMfb.yOCuXd.qjTEB > div > div > span:nth-child(11)',
+                        element: null
                     }
                 }
             },
@@ -88,46 +128,104 @@ class MeetActions {
         chrome.runtime.onConnect.addListener((port) => {
             console.log('onConnect');
             this.port = port;
+            this.port.onDisconnect.addListener(() => {
+                console.log('port disconnected');
+                delete this.port;
+            });
             this.port.onMessage.addListener(this.handleMessage_);
         });
+        this.addStateDetection_();
         chrome.runtime.sendMessage({action: 'addTab'});
     }
 
     addStateDetection_() {
-
+        const detectState = () => {
+            let windowState;
+            for (let [state, detector] of Object.entries(this.stateDetectors)) {
+                if (detector()) {
+                    windowState = state;
+                    break;
+                }
+            }
+            if (windowState && windowState !== this.currentState) {
+                console.log(`state change ${this.state} => ${windowState}`);
+                this.configureForState_(windowState);
+            }
+        }
+        setInterval(() => (detectState()), 1000);
+        detectState();
+        console.log('state detection added');
     }
 
     configureForState_(state) {
-        console.log('configureForState', this.currentState, state);
-        if (state.videoOnHold && this.currentState !== 'onHold') {
-            console.log('on hold');
-            this.currentState = 'onHold';
-            Object.entries(this.holdControls).forEach(([key, ctrl]) => {
-                this.addElementDetector_(key, ctrl);
-            });
-        } else if (state.videoOnPage && this.currentState !== 'onAir') {
-            console.log('on air');
-            this.currentState = 'onAir';
-            Object.entries(this.meetControls).forEach(([key, ctrl]) => {
-                this.addElementDetector_(key, ctrl);
-            });
-        } else if (this.currentState !== 'onHome') {
-            console.log('on home');
-            this.currentState = 'onHome';
-            Object.entries(this.homeControls).forEach(([key, ctrl]) => {
-                this.addElementDetector_(key, ctrl);
+        console.log('configureForState', state);
+        if (this.currentState) {
+            Object.entries(this.controls[this.currentState]).forEach(([key, ctrl]) => {
+                delete ctrl.element;
             });
         }
+        Object.entries(this.controls[state]).forEach(([key, ctrl]) => {
+            this.addElementDetector_(key, ctrl);
+        });
+        this.currentState = state;
     }
 
-    handleMessage_(message, port) {
+    async executeCommand(command) {
+        const keyPath = command.split('.');
+        console.log(keyPath);
+        let ctrls = this.controls[this.currentState];
+        for (const k of keyPath) {
+            const ctrl = ctrls[k];
+            ctrl.element = document.querySelectorAll(ctrl.selector)[0];
+            if (!ctrl || !ctrl.element) {
+                return false;
+            }
+            console.log('clickling', k, ctrl.element);
+            ctrl.element.click();
+            if (ctrl.children) {
+                ctrls = ctrl.children;
+            }
+            
+        }
+        return true;
+    }
+
+    handleMessage_ = async (message, port) => {
         try {
             console.log('handleMessage', message);
-            port.postMessage(Object.assign(message, {success: true}));
+            let success = true;
+            if (message.command) {
+                success = await this.executeCommand(message.command);
+            } else if (message.action === 'addTab') {
+                success = this.addStateDetection_();
+            }
+            port.postMessage(Object.assign({success}, message));
         } catch(e) {
-            console.error(e.message);
-            port.postMessage(Object.assign(message, {success: false}));
+            console.warn(e);
+            port.postMessage(Object.assign({success: false}, message));
         }
         
+    }
+
+    async addElementDetector_(key, control) {
+        let attempts = 0;
+        while (attempts < 5) {
+            const els = document.querySelectorAll(control.selector);
+            if (els.length) {
+                control.element = els[0];
+                if (els.length > 1) {
+                    console.warn('too broad selector', control.selector);
+                    console.log(els);
+                }
+                break;
+            }
+            attempts++;
+            await new Promise((resolve, reject) => setTimeout(resolve, 500 * 2 ** attempts));
+        }
+        console.log(`${key} element configured`, control.element);
+        if (this.port) {
+            this.port.postMessage({message: 'listenerAdded', key, success: true});
+        }
+        return control.element;
     }
 }
